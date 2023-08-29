f0d2074645c643292ef1820413ce1b3e1a00c8f8
liz-ab <liz-ab@glodon.com>
2023-08-29 23:14:53 +0800

    webpack split code

---
+++ webpackSplitDemo/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: "./src/app.js",
+        main: "./src/main.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public/index.html"),
+        })
+    ],
+    clean: true,
+    mode: "production",
+}
