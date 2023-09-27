aa809e3a8eabd0f916a99eb503a1d5b4e0739c21
liz-ab <liz-ab@glodon.com>
2023-09-27 22:43:11 +0800

    chore

--- threeDemo/src/index.js
+++ threeDemo/src/index.js
@@ -1,51 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Viewport } from "./viewport";
 
-let camera, scene, renderer, labelRenderer;
-
-init();
-animate();
-
-function init() {
-
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 200 );
-    camera.position.set( 10, 5, 20 );
-    camera.up.set(0, 0, 1);
-    scene = new THREE.Scene();
-
-    const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
-    dirLight.position.set( 0, 0, 1 );
-    dirLight.layers.enableAll();
-    scene.add( dirLight );
-
-    const axesHelper = new THREE.AxesHelper( 5 );
-    axesHelper.layers.enableAll();
-    scene.add( axesHelper );
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.minDistance = 5;
-    controls.maxDistance = 100;
-    window.addEventListener( 'resize', onWindowResize );
-    controls.addEventListener('change', animate);
-
-    // Api test
-    const box1 = new THREE.Box2();
-    const box2 = new THREE.Box2(new THREE.Vector2(-1, -1), new THREE.Vector2(3, 3));
-    console.log(box2.distanceToPoint(new THREE.Vector2(0, 0)));
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-function animate() {
-    // requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-}
+const container = document.getElementById('view');
+const view = new Viewport(container);
+view.refresh();
---
+++ threeDemo/src/viewport.js
@@ -0,0 +1,46 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+class Viewport {
+    constructor(container) {
+
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xfafa00);
+
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 200);
+        this.camera.position.set(10, 5, 20);
+        this.camera.up.set(0, 0, 1);
+    
+        this.dirLight = new THREE.DirectionalLight(0xffffff, 3);
+        this.dirLight.position.set(0, 0, 1);
+        this.scene.add(this.dirLight);
+    
+        this.axesHelper = new THREE.AxesHelper(5);
+        this.scene.add(this.axesHelper);
+    
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        container.appendChild(this.renderer.domElement);
+    
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.minDistance = 5;
+        this.controls.maxDistance = 100;
+        window.addEventListener('resize', this.onWindowResize);
+        this.controls.addEventListener('change', this.refresh);    
+    }
+
+    refresh = () => {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    onWindowResize = () => {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+}
+
+export { Viewport };
