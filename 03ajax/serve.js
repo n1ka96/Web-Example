890b1dcb848ec5304ed7cb441935c3d425221882
liz-ab <liz-ab@glodon.com>
2024-02-21 11:16:58 +0800

    fix: ajax test

--- 03ajax/serve.js
+++ 03ajax/serve.js
@@ -4,7 +4,22 @@ const app = express();
 
 app.get('/serve', (request, response) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
-    response.send('hello');
+    const data = {
+        name:'ageguigu',
+        age:18,
+        gender:'男'
+    }
+    let str = JSON.stringify(data);
+    response.send(str);
+    // setTimeout(function() {
+    //     response.send('delay')
+    // }, 3000)
+})
+
+app.post('/serve', (request, response) => {
+    response.setHeader('Access-Control-Allow-Headers','*')
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.send('HELLO AJAX POST');
 })
 
 app.listen(8000, () => {
